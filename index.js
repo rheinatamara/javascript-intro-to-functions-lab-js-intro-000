c9f02f4648718da382f73a0b8197f3ce8b2da56a
Rheina <rheina.tamara@outlook.com>
2021-04-27 05:00:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,13 @@ function logWhisper(string){
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string){
-    if(string === string.lowerCase()){
-      return 'I can\'t hear you!'
-    }
+  if (string === string.toLowerCase()){
+    return `I can't hear you!`;
+  }
+  else if (string === string.toUpperCase()){
+    return `YES INDEED!`
+  }
+  else if (string === "I love you, Grandma."){
+    return `I love you, too.`
+  }
 }
