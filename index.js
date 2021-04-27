02274ee8f488fadff2a399a6c1bd705ca6a98bcd
Rheina <rheina.tamara@outlook.com>
2021-04-27 04:56:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,9 @@ function logWhisper(string){
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string){
-  
+  if(string.lowerCase()){
+    return 'I can\'t hear you!'
+  } else if (string.uppercase()){
+    return 'YES INDEED!';
+  }
 }
