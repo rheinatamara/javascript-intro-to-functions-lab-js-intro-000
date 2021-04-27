54a7f5eb24d6d24b88e9e870607b521929430018
Rheina <rheina.tamara@outlook.com>
2021-04-27 04:51:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-function shout(string){
+\function shout(string){
   return string.toUpperCase();
 }
 function whisper(string){
   return string.toLowerCase();
 }
 function logShout(string){
-  console.log(string.toLowerCase());
+  console.log(string.toUpperCase());
 }
